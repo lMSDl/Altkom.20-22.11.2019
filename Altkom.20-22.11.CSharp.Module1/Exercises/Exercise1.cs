51e598886bfb4d168b8deb8987832165c43399b8
Paweł Łęczycki <pawel@msd.info.pl>
2019-11-20 08:58:31 +0100

    Module 1 Exercise 1

---
+++ Altkom.20-22.11.CSharp.Module1/Exercises/Exercise1.cs
@@ -0,0 +1,66 @@
+﻿using Altkom._20_22._11.CSharp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Altkom._20_22._11.CSharp.Exercises
+{
+    public class Exercise1
+    {
+        private IList<Person> Persons { get; }
+        private string _lastOutout;
+
+        public Exercise1()
+        {
+            Persons = new List<Person> { new Person { Id = 1, BirthDate = new DateTime(1990, 12, 3), Gender = 0, FirstName = "Ewa", LastName = "Adamska" },
+                new Person { Id = 1, BirthDate = new DateTime(1988, 8, 21), Gender = 1, FirstName = "Adam", LastName = "Adamska" } };
+        }
+
+        public void Start()
+        {
+            ShowPersons();
+            while (ReadCommand(Console.ReadLine()))
+            {
+            }
+        }
+
+        public void ShowPersons()
+        {
+            WriteLine(Persons.OrderBy(x => x.LastName).Select(x => x.ToString()).Aggregate((a, b) => $"{a}\n{b}"));
+        }
+
+        public bool ReadCommand(string input)
+        {
+            //TODO 1 Jeśli użytkownik wpisze "edit {id}", gdzie {id} to identyfikator osoby, wyszukać tę osobę na liście i uruchomić funkcję EditPerson
+            //TODO 2 Po wprowadzeniu zmian, wyświetlić ponownie listę osób
+
+            switch (input)
+            {
+                case "exit":
+                    return false;
+                default:
+                    WriteLine(_lastOutout);
+                    break;
+            }
+            return true;
+        }
+
+        public void EditPerson(Person person)
+        {
+            WriteLine(nameof(Person.FirstName));
+            SendKeys.SendWait(person.FirstName);
+            person.FirstName = Console.ReadLine();
+
+            //TODO 3 Uzupełnić kod o edycję Person.LastName oraz Person.BirthDate
+        }
+
+        private void WriteLine(string outout)
+        {
+            Console.Clear();
+            Console.WriteLine(outout);
+            Console.WriteLine();
+            _lastOutout = outout;
+        }
+    }
+}
---
+++ Altkom.20-22.11.CSharp.Module1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Altkom._20_22._11.CSharp.Exercises;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._20_22._11.CSharp.Module1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Exercise1().Start();
+        }
+    }
+}
