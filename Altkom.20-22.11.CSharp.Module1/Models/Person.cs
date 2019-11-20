847fd93a626d8b40b540a40210067156f4fe2664
Paweł Łęczycki <pawel@msd.info.pl>
2019-11-20 08:58:31 +0100

    Module 1 Exercise 1

---
+++ Altkom.20-22.11.CSharp.Module1/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Altkom._20_22._11.CSharp.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Gender { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0,-3} {1,-15} {2,-15} {3,-10}", Id, LastName, FirstName, BirthDate.ToShortDateString());
+        }
+    }
+}
