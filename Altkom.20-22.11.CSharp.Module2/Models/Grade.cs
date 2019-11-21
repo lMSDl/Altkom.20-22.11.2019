544ae9df4f083eec3e22e8088acd22ef68e100f8
Paweł Łęczycki <pawel@msd.info.pl>
2019-11-21 14:41:27 +0100

    Module 2 Exercise 10 Solution

--- Altkom.20-22.11.CSharp.Module2/Models/Grade.cs
+++ Altkom.20-22.11.CSharp.Module2/Models/Grade.cs
@@ -1,4 +1,6 @@
-﻿namespace Altkom._20_22._11.CSharp.Module2.Models
+﻿using Newtonsoft.Json;
+
+namespace Altkom._20_22._11.CSharp.Module2.Models
 {
     public class Grade
     {
@@ -7,6 +9,11 @@
         public string SubjectName { get; set; }
         public string Assessment { get; set; }
         public string Comments { get; set; }
+
+        public bool ShouldSerializeComments()
+        {
+            return Assessment != "A+";
+        }
 
         //ctrl+. => generate constructor
         public Grade(string assessmentDate, string subjectName, string assessment, string comments)
--- Altkom.20-22.11.CSharp.Module2/Views/StudentProfileView.xaml.cs
+++ Altkom.20-22.11.CSharp.Module2/Views/StudentProfileView.xaml.cs
@@ -2,11 +2,13 @@
 using Altkom._20_22._11.CSharp.Module2.Models;
 using Altkom._20_22._11.CSharp.Module2.Services;
 using Microsoft.Win32;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-//TODO 10.2a: Dodaj przestrzeń nazw Newtonsoft.Json
 
 namespace Altkom._20_22._11.CSharp.Module2.Views
 {
@@ -92,26 +94,29 @@ namespace Altkom._20_22._11.CSharp.Module2.Views
         {
             try
             {
-                //TODO 10.1: Pobierz listę ocen aktualnie wybranego studenta
-
-                //TODO 10.2b: Przeprowadź serializację do JSON
-
-                //TODO 10.3a: Pokaż raport w postaci MessageBox i zadaj pytanie użytkownikowi czy chce zapisać raport 
-
-
-
+                var grades = DataSource.Grades.Where(g => g.StudentID == SessionContext.CurrentStudent.StudentID).ToList();
+                var output = JsonConvert.SerializeObject(grades, Formatting.Indented);
+                
+                var reply = MessageBox.Show(output, "Save Report?", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (reply == MessageBoxResult.Yes)
+                {
                     var dialog = new SaveFileDialog
                     {
-                        Filter = "JSON documents|*.json",
-                        FileName = "Grades",
-                        DefaultExt = ".json"
+                        Filter = "JSON documents|*.json|XML documents|*.xml",
+                        FileName = "Grades"
                     };
-                //TODO 10.3b: Pokaż SaveFileDialog i zapisz rezultat do zmiennej lokalnej
+                    var result = dialog.ShowDialog();
 
-                //TODO 10.3c: W przypadku pozytywnej odpowiedzi, zapisz raport do pliku używająć FileStream
+                    if (result.HasValue && result.Value)
+                    {
+                        if(dialog.FileName.EndsWith(".xml"))
+                            output = JsonConvert.DeserializeXNode("{\"Grade\": " + output + " }", "Grades").ToString();
 
-                //TODO 10.3d: Zwolnij zasoby klasy strumieniowej
-
+                        var file = new FileStream(dialog.FileName, FileMode.Create, FileAccess.Write);
+                        using (var streamWriter = new StreamWriter(file))
+                                streamWriter.Write(output);
+                    }
+                }
             }
             catch (Exception ex)
             {
@@ -143,7 +148,16 @@ namespace Altkom._20_22._11.CSharp.Module2.Views
 
         private void LoadReport_Click(object sender, RoutedEventArgs e)
         {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "JSON documents|*.json";
+            bool? result = dialog.ShowDialog();
 
+            if (result.HasValue && result.Value)
+            {
+                string gradesAsJson = File.ReadAllText(dialog.FileName);
+                var gradeList = JsonConvert.DeserializeObject<List<Grade>>(gradesAsJson);
+                studentGrades.ItemsSource = gradeList;
+            }
         }
     }
 }
